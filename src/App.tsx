8985a73d75f69405c616ebf589ac8d008154adad
petrivym <petriv.dev@gmail.com>
2021-04-27 18:58:31 +0300

    React 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import CharacterComponent from "./components/characterComponent";
+import ArticleComponent from "./components/article/articleComponent";
 
 function App() {
     return (
         <div>
-            <CharacterComponent description={'bart'} img ={"https://upload.wikimedia.org/wikipedia/uk/a/aa/Bart_simpson.png"}/>
-            <CharacterComponent description={'lisa'} img ={"https://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png"}/>
+            <div className={'content-block'}>
+                <div className={'container'}>
+                    <div className={'content-block__items'}>
+                        <ArticleComponent
+                            linksImg={'https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg'}
+                            title={'Цитати про рішучість'}
+                            article={'Якщо ви йдете крізь пекло – ідіть, не зупиняючись.'}
+                        />
+                        <ArticleComponent
+                            linksImg={'https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg'}
+                            title={'Цитати про позицію'}
+                            article={'У тебе є вороги? Добре. Отже, в своєму житті ти щось колись відстоював.'}
+                        /> <ArticleComponent
+                        linksImg={'https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg'}
+                        title={'Цитати про успіх '}
+                        article={'Успіх – це здатність крокувати від однієї невдачі до іншої, не втрачаючи ентузіазму.'}
+                    /> <ArticleComponent
+                        linksImg={'https://icdn.lenta.ru/images/2019/09/07/18/20190907182940553/square_320_4a4daec12981891d5b0270fe7a4de168.jpg'}
+                        title={'Цитати про можливості'}
+                        article={'Будь-яка криза – це нові можливості.'}
+                    />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
---
+++ src/components/article/articleComponent.js
@@ -0,0 +1,26 @@
+import React from "react";
+import './articleComponent.css'
+
+
+function ArticleComponent(props) {
+    return <div className={'content-block__item'}>
+        <div className={'content-block__wraper'}>
+            <div className={'content-block__img'}>
+                <img
+                    src={props.linksImg}
+                    alt="img"/>
+            </div>
+            <div className={'content-block__title'}>
+                <p>{props.title}</p>
+            </div>
+            <div className={'content-block__list'}>
+                <ul className={'content-block__ul'}>
+                    <li>{props.article} </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+}
+
+
+export default ArticleComponent;
